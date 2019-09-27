eeb17ac70027bc563b3b16407b79dbe890119392
dantin <chengjie.ding@gmail.com>
2019-09-27 15:23:41 +0800

    add discovery server.

---
+++ microservices/support/discovery-server/src/test/java/com/github/dantin/webster/support/discovery/TestOnlyApplication.java
@@ -0,0 +1,10 @@
+package com.github.dantin.webster.support.discovery;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOnlyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestOnlyApplication.class, args);
+  }
+}
