05d31fdb8c3856985719ceeb366e8e4c31e519e6
dantin <chengjie.ding@gmail.com>
2019-09-27 16:18:07 +0800

    add gateway server.

---
+++ microservices/support/gateway-server/src/test/java/com/github/dantin/webster/support/gateway/TestOnlyApplication.java
@@ -0,0 +1,10 @@
+package com.github.dantin.webster.support.gateway;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOnlyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TestOnlyApplication.class, args);
+  }
+}
